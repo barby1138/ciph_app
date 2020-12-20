51ca6f7f74da5ddc5d80284834d9e8ba82afd66a
tsis <tsis@tsis.com>
2020-12-20 15:21:34 +0100

    update

--- ciph_app_test/build.sh
+++ ciph_app_test/build.sh
@@ -2,4 +2,4 @@
 gcc -o ciph_app_test ciph_app_test.c \
         -I../include \
         -L../lib \
-        -lpthread -lrt -lciph_agent -lmemif_client -lstdc++
+        -lpthread -lrt -lciph_agent -lmemif_client -lstdc++ -std=c11
