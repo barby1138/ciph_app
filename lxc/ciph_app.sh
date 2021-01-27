f7d6fc4273ba560b796d8f270cf91fbdf6ad15f8
tsis <tsis@tsis.com>
2021-01-27 22:35:51 +0100

    upd

--- lxc/ciph_app.sh
+++ lxc/ciph_app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "htg.service: ## Starting ##" | systemd-cat -p info
+echo "ciph_app.service: ## Starting ##" | systemd-cat -p info
+./dpdk-crypto-app cfg
 
-while :
-do
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-echo "htg.service: timestamp ${TIMESTAMP}" | systemd-cat -p info
-sleep 60
-done
+#while :
+#do
+#TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+#echo "ciph_app.service: timestamp ${TIMESTAMP}" | systemd-cat -p info
+#sleep 60
+#done
 
