b4cf58fa5771ae70286b75a142abff07381c9aa2
tsis <tsis@tsis.com>
2021-01-28 21:34:33 +0100

    upd

--- ciph_app_test/build.sh
+++ ciph_app_test/build.sh
@@ -1,5 +1,8 @@
 
 gcc -o ciph_app_test ciph_app_test.c \
+        -mavx2 -std=c11 \
         -I../include \
         -L../lib \
         -lpthread -lrt -lciph_agent
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../lib
