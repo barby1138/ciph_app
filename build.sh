4e8805489aa60441b1d42930294405b77f6e8784
tsis <tsis@tsis.com>
2021-02-09 10:00:38 +0100

    upd

--- build.sh
+++ build.sh
@@ -4,20 +4,20 @@ echo $ROOT
 VER=$(cat VERSION)
 echo ${VER}
 
-echo ============== COPY ARTIFACTORY =====================
+echo ============== COPY ARTIFACTORY ==================
 cd $ROOT
 mkdir 3rdparty_artifactory
 cp /tmp/dpdk-20.05-x86_64-native-linuxapp-gcc.tar.gz 3rdparty_artifactory
 #cp /tmp/rootfs_centos-7-amd64.tar.gz 3rdparty_artifactory
 cp /tmp/libIPSec_MB.0.54.0.tar.gz 3rdparty_artifactory
 
-echo ============== PREP IPSec =====================
+echo ============== PREP IPSec ========================
 cd $ROOT
 cd 3rdparty_artifactory
 tar zxf libIPSec_MB.0.54.0.tar.gz
 cp -f libIPSec_MB* /usr/lib
 
-echo ============== PREP DPDK =====================
+echo ============== PREP DPDK =========================
 cd $ROOT
 cd 3rdparty_artifactory
 tar zxf dpdk-20.05-x86_64-native-linuxapp-gcc.tar.gz -C $ROOT/3rdparty
@@ -33,12 +33,12 @@ cd $ROOT
 cd 3rdparty/fridmon-0.1.10/hyperon_1_0_1/project/redhat
 make CFG=release
 
-echo ============== BUILD CIPH_APP =====================
+echo ============== BUILD CIPH_APP ====================
 cd $ROOT
 cd project/linux
 make CFG=release
 
-echo =========== PREP DEVEL ==============
+echo ============== PREP DEVEL ========================
 cd $ROOT
 mkdir dist/ciph_app_devel
 mkdir dist/ciph_app_devel/include
@@ -56,11 +56,11 @@ cp ciph_app_test/build.sh dist/ciph_app_devel/examples
 cp VERSION dist/ciph_app_devel
 cp dist/readme dist/ciph_app_devel
 
-echo =========== PACK DEVEL ==============
+echo =========== PACK DEVEL ===========================
 cd $ROOT/dist
 tar czf ciph_app_devel-$VER.tar.gz ciph_app_devel
 
-echo =========== PREP FILES FOR RPM ==============
+echo =========== PREP FILES FOR RPM ===================
 RPMBUILD=~/rpmbuild/BUILD
 cd $ROOT
 echo $RPMBUILD
@@ -83,10 +83,10 @@ cp 3rdparty_artifactory/libIPSec_MB* $RPMBUILD/ciph_app/lib
 mkdir $RPMBUILD/ciph_app/lxc
 cp /tmp/rootfs_centos-7-amd64.tar.gz $RPMBUILD/ciph_app/lxc
 
-echo =========== BUILD RPM ==============
+echo =========== BUILD RPM ============================
 cd $ROOT/lxc
 rpmbuild -bb --define "_ver $VER" ciph_app.spec
 
-echo =========== PACK RPM ==============
+echo =========== PACK RPM =============================
 cd $ROOT/dist
 cp ~/rpmbuild/RPMS/x86_64/*.rpm .
