c9ce53d9f161b410d4687f59ce75a89135ee7364
tsis <tsis@tsis.com>
2021-02-11 12:57:56 +0100

    upd

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 #FROM centos:7.4.1708
-FROM pwartifactory.parallelwireless.net/pw-docker-images/base-docker-images/devops-docker-prod-centos-7.4.1708-base-v1:latest
+FROM pwartifactory.parallelwireless.net/devops_dockerhub/base-docker-images/devops-docker-prod-centos-7.4.1708-base-v2:latest
 
-#RUN yum update 
+#RUN yum update
 RUN yum install -y wget
-RUN yum install -y python3 python3-pip
+RUN yum install -y git
+RUN yum install -y e python3 python3-pip
 RUN pip3 install gdown
 RUN yum install -y rpm-build redhat-rpm-config
 #RUN yum group install -y "Development Tools"
 RUN yum install -y numactl-devel expat-devel
 RUN yum install -y gcc gcc-c++ make
+
+RUN mkdir -p /opt/swtools/bin/depot_tools/jfrog_CLI/ && \
+    wget https://pwartifactory.parallelwireless.net:443/artifactory/cws-toolschains-remote-cache/jfrog -P /opt/swtools/bin/depot_tools/jfrog_CLI/
 
 RUN cd /tmp && gdown https://drive.google.com/uc?id=1LtqOrPmxLV1KCNG5H5pU_zzqFPlK69W3
 RUN cd /tmp && gdown https://drive.google.com/uc?id=1LsHbeX0M4qLBXPqp9JTF6UwmqmBizJXW
@@ -16,9 +20,11 @@ RUN cd /tmp && gdown https://drive.google.com/uc?id=1ZtRmUM9nZg594LIMMbUL9fFBCLj
 
 #creating user
 ARG UNAME=parallel
-#ARG UID=1000
-#ARG GID=1000
+ARG UID=1000
+ARG GID=1000
+#RUN echo $UNAME $UID $GID
 #RUN groupadd -g $GID -o $UNAME
 #RUN useradd -m -u $UID -g $GID -o -s /bin/bash $UNAME
 #RUN usermod -aG root $UNAME
 #USER $UNAME
+
--- ci-publish.sh
+++ ci-publish.sh
@@ -4,36 +4,23 @@ echo "Executing ci-publish.sh ..."
 
 vmname=$(hostname)
 uname=$(whoami)
-echo "Current git commit: ${GIT_COMMIT}"
-GIT_COMMIT=$(git rev-parse HEAD)
-echo "Actual git commit: ${GIT_COMMIT}"
-HASH=${GIT_COMMIT:0:8}
-echo $HASH
-#BRANCH=${BUILD_DISPLAY_NAME}
-echo "Current branch name: ${BRANCH_NAME}; ${BUILD_DISPLAY_NAME}"
-BRANCH=${BRANCH_NAME}
-UBRANCH=$(echo $BRANCH | sed 's/\//:/g')
-DATE=$(git log --format=format:%cd --date=format:%Y%m%d.%H%M -n 1)
+commit_hash_date=$(git log --format=format:%cd-%h --date=format:%Y%m%d.%H%M -n 1)
+shortHash=$(git rev-parse --short HEAD)
+buildNumber=$BUILD_NUMBER
+
+branch=${push_changes_0_new_name}
+#branch=develop
 
 export PATH=$PATH:/opt/swtools/bin/depot_tools/jfrog_CLI/
 
 upload_to_artifactory()
 {
-    local image_name="${1}"
-
-    echo "Publish file: $image_name"
-    tfname="${image_name%.*}"
-    tfext="${image_name##*.}"
-    GZ_DATA=${UBRANCH}:${DATE}-${HASH}:$tfname.${DATE}-${HASH}
-    tfile=${GZ_DATA}.$tfext
-    FSTRING=$(echo $tfile | sed 's/:/\//g')
-    MYDIR=$(dirname ${FSTRING})/
-    MYFILE=$(basename ${FSTRING})
-    mv $image_name "$MYFILE"
-    echo "To publish '$MYFILE' into dir '$MYDIR'"
-    echo "please the find the tgz files in dist dir !"
+    local p_image_name="${1}"
+    local p_folder_to_upload="pw-products/ciph-app/${branch}/${commit_hash_date}/"
+    echo "Upload ${p_image_name} to folder ${p_folder_to_upload} with the tag $shortHash"
     jfrog rt use pwartifactory
-    jfrog rt u --props "commitID=${HASH}" $MYFILE pw-products/ciph-app/"$MYDIR"
+    jfrog rt u --insecure-tls --props "commitID=$shortHash" --props "build.number=$buildNumber" --include-dirs=true \
+          ${p_image_name} ${p_folder_to_upload}
 }
 
 echo "Checking current folder and content"
