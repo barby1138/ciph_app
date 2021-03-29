8f655b565e9801dab8a338b14fdb1e3ae474e776
tsis <tsis@tsis.com>
2021-03-29 16:07:29 +0200

    upd

--- cont/k8s/build_k8s.sh
+++ cont/k8s/build_k8s.sh
@@ -11,10 +11,16 @@ docker build -t ciph_app:v$VER -f Dockerfile_ciph_app .
 docker build -t ciph_app_test:v$VER -f Dockerfile_ciph_app_test .
 
 cd $K8S_ROOT/deploy/helm/ciph_app_chart
-./gen_values.sh $VER
+./gen_chart.sh $VER
 cd $K8S_ROOT/deploy/helm/ciph_app_test_chart
-./gen_values.sh $VER
+./gen_chart.sh $VER
+
+cd $K8S_ROOT/deploy/helm/ciph_app_chart
+tar czf ciph_app-$VER.tgz ciph_app
 
 cd $K8S_ROOT/deploy/helm
-mkdir ciph_app_test_chart/charts
-cp -R ciph_app_chart ciph_app_test_chart/charts
+mkdir ciph_app_test_chart/ciph_app_test/charts
+cp ciph_app_chart/ciph_app-$VER.tgz ciph_app_test_chart/ciph_app_test/charts
+
+cd $K8S_ROOT/deploy/helm/ciph_app_test_chart
+tar czf ciph_app_test-$VER.tgz ciph_app_test
--- cont/k8s/deploy/helm/ciph_app_test_chart/gen_chart.sh
+++ cont/k8s/deploy/helm/ciph_app_test_chart/gen_chart.sh
@@ -1,10 +1,18 @@
+VER=$1
 TAG=v$1
 
 cat > values.yaml <<EOF
 # replicaCount number of replicas 
 replicaCount: 1
 # repository docker repo name
-repository: ciph_app_test
+repository: ciph_app
 # tag docker docker image tag 
 tag: $TAG
+EOF
+
+cat > Chart.yaml <<EOF
+apiVersion: v2
+name: ciph_app
+description: A Helm chart for Kubernetes
+version: $VER
 EOF
