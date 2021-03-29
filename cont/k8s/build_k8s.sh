2c0cf850a5a3a43f92531255618ae8d9dc5f9582
tsis <tsis@tsis.com>
2021-03-29 16:12:38 +0200

    upd

--- cont/k8s/build_k8s.sh
+++ cont/k8s/build_k8s.sh
@@ -2,6 +2,8 @@
 K8S_ROOT=$PWD 
 echo $K8S_ROOT
 
+K8S_DEPLOY=$K8S_ROOT/deploy/helm
+
 cd $K8S_ROOT
 
 VER=$(cat ciph_app/VERSION)
@@ -10,17 +12,17 @@ echo $VER
 docker build -t ciph_app:v$VER -f Dockerfile_ciph_app .
 docker build -t ciph_app_test:v$VER -f Dockerfile_ciph_app_test .
 
-cd $K8S_ROOT/deploy/helm/ciph_app_chart
+cd $K8S_DEPLOY/ciph_app_chart
 ./gen_chart.sh $VER
-cd $K8S_ROOT/deploy/helm/ciph_app_test_chart
+cd $K8S_DEPLOY/ciph_app_test_chart
 ./gen_chart.sh $VER
 
-cd $K8S_ROOT/deploy/helm/ciph_app_chart
+cd $K8S_DEPLOY/ciph_app_chart
 tar czf ciph_app-$VER.tgz ciph_app
 
-cd $K8S_ROOT/deploy/helm
+cd $K8S_DEPLOY
 mkdir ciph_app_test_chart/ciph_app_test/charts
 cp ciph_app_chart/ciph_app-$VER.tgz ciph_app_test_chart/ciph_app_test/charts
 
-cd $K8S_ROOT/deploy/helm/ciph_app_test_chart
+cd $K8S_DEPLOY/ciph_app_test_chart
 tar czf ciph_app_test-$VER.tgz ciph_app_test
