fd591d9546e0e88539a7033cff476e967eec7e38
tsis <tsis@tsis.com>
2021-03-29 16:31:39 +0200

    upd

--- cont/k8s/deploy/helm/ciph_app_chart/gen_chart.sh
+++ cont/k8s/deploy/helm/ciph_app_chart/gen_chart.sh
@@ -1,18 +1,18 @@
 VER=$1
 TAG=v$1
 
-cat > ciph_app_test/values.yaml <<EOF
+cat > ciph_app/values.yaml <<EOF
 # replicaCount number of replicas 
 replicaCount: 1
 # repository docker repo name
-repository: ciph_app_test
+repository: ciph_app
 # tag docker docker image tag 
 tag: $TAG
 EOF
 
-cat > ciph_app_test/Chart.yaml <<EOF
+cat > ciph_app/Chart.yaml <<EOF
 apiVersion: v2
-name: ciph_app_test
+name: ciph_app
 description: A Helm chart for Kubernetes
 version: $VER
 EOF
--- cont/k8s/deploy/helm/ciph_app_test_chart/gen_chart.sh
+++ cont/k8s/deploy/helm/ciph_app_test_chart/gen_chart.sh
@@ -1,18 +1,18 @@
 VER=$1
 TAG=v$1
 
-cat > ciph_app/values.yaml <<EOF
+cat > ciph_app_test/values.yaml <<EOF
 # replicaCount number of replicas 
 replicaCount: 1
 # repository docker repo name
-repository: ciph_app
+repository: ciph_app_test
 # tag docker docker image tag 
 tag: $TAG
 EOF
 
-cat > ciph_app/Chart.yaml <<EOF
+cat > ciph_app_test/Chart.yaml <<EOF
 apiVersion: v2
-name: ciph_app
+name: ciph_app_test
 description: A Helm chart for Kubernetes
 version: $VER
 EOF
