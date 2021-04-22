168612fe34f1999afa7a911955c59a5016ba3cc4
tsis <tsis@tsis.com>
2021-04-22 16:10:35 +0200

    upd

--- cont/k8s/build_k8s.sh
+++ cont/k8s/build_k8s.sh
@@ -9,8 +9,11 @@ cd $K8S_ROOT
 VER=$(cat ciph_app/VERSION)
 echo $VER
 
-docker build -t ciph_app:v$VER -f Dockerfile_ciph_app .
-docker build -t ciph_app_test:v$VER -f Dockerfile_ciph_app_test .
+#tmp
+docker build -t olitsis/ciph_app:v$VER -f Dockerfile_ciph_app .
+docker build -t olitsis/ciph_app_test:v$VER -f Dockerfile_ciph_app_test .
+echo "Boris220813" | docker login --username olitsis --password-stdin
+docker push olitsis/ciph_app:v$VER
 
 cd $K8S_DEPLOY/ciph_app_chart
 chmod +x gen_chart.sh && ./gen_chart.sh $VER
